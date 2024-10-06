c009a374515d58185a1d4d285194db8b9b58d1f1
ferneediaz <dandiaz10@yahoo.com>
2024-10-06 17:05:31 +0800

    DRAFT COMMIT SIGNUP NOT FINISHED

---
+++ server/controllers/auth.controller.js
@@ -0,0 +1,55 @@
+    import bcryptjs from 'bcryptjs';
+    import { User } from '../models/user.model.js'
+    import bcrypt from 'bcryptjs';
+    import { generateTokenAndSetCookie } from '../utils/generateTokenAndSetCookie.js';
+
+    export const signup = async (req, res ) => {
+    const {email,password,name} = req.body;
+    console.log("Headers: ", req.headers);  // Log headers
+    console.log("Body: ", req.body);        // Log body
+
+    try{
+    if (!email || !password || !name) { // fix typo here
+    throw new Error("All fields are required");
+    }
+
+    const userAlreadyExists = await User.findOne({email});
+    
+
+    if(userAlreadyExists){
+        return res.status(400).json({success:false, message: "User already exists"});
+    }
+    const hashedPassword = await bcryptjs.hash(password,10);
+    const verificationToken = Math.floor(100000 + Math.random() * 900000).toString();
+    const user = new User({
+        email,
+        password: hashedPassword,
+        name,
+        verificationToken,
+        verificationTokenExpiresAt: Date.now( + 24 * 60 ^ 60 *1000)
+    })
+
+    await user.save();
+
+    generateTokenAndSetCookies(res,user._id)
+
+    res.status(201).json({
+        succes: true,
+        message: "User created succesfully",
+        user:{
+            ...user._doc,
+            password: undefined,
+        }
+    })
+
+    } catch (error) {
+        res.status(400).json({success:false, message: error.message})
+    }
+    
+    }
+    export const login = async (req, res ) => {
+        res.send("login route");
+    }
+    export const logout = async (req, res ) => {
+        res.send("logout route");
+    }
--- server/index.js
+++ server/index.js
@@ -1,20 +1,20 @@
 import express from "express";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.get("/",(req,res) => {
-    res.send("Hello123 world")
-});
+// Allows parsing of incoming requests with JSON payloads
+app.use(express.json()); 
 
-app.listen(3000, () => {
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running on port 3000")
+    console.log("Server is running on port ", PORT);
 });
-
-
-
