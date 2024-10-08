71d70a9f83d2378fe285b6d0bd75c14336f10524
ferneediaz <dandiaz10@yahoo.com>
2024-10-08 23:26:31 +0800

    configuration for mailtrap

--- server/mailtrap/mailtrap.config.js
+++ server/mailtrap/mailtrap.config.js
@@ -4,28 +4,11 @@ dotenv.config();
 const TOKEN = process.env.MAILTRAP_TOKEN
 const ENDPOINT = process.env.MAILTRAP_ENDPOINT
 
-const client = new MailtrapClient({
-  endpoint: ENDPOINT,
-  token: TOKEN
+export const client = new MailtrapClient({
+  endpoint: TOKEN,
+  token: ENDPOINT});
 
-});
-
-const sender = {
+export const sender = {
   email: "mailtrap@demomailtrap.com",
-  name: "Mailtrap Test",
+  name: "Dan",
 };
-const recipients = [
-  {
-    email: "dandiaz10@yahoo.com",
-  }
-];
-
-client
-  .send({
-    from: sender,
-    to: recipients,
-    subject: "You are awesome!",
-    text: "Congrats for sending test email with Mailtrap!",
-    category: "Integration Test",
-  })
-  .then(console.log, console.error);
